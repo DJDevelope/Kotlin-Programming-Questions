56d1f3144a97a062855df167c98b17710efc277e
dhananjay <dhananjaysingh271@gmail.com>
2025-04-26 22:33:11 +0530

    Program to check anagram of two strings.

--- app/src/main/java/com/example/programs/coding/check_string_is_anagram.kt
+++ app/src/main/java/com/example/programs/coding/check_string_is_anagram.kt
@@ -1,10 +1,23 @@
+/*
+Simple Anagram Pairs:
+listen → silent
+evil → vile
+dusty → study
+save → vase
+angel → glean
+elbow → below
+night → thing
+inch → chin
+earth → heart
+brag → grab
+*/
 //Check if two strings are Anagrams
 fun main() {
     println("Enter first string")
     //Read first string from the user and remove the space from the string
     val firstStr = readln().replace(" ","")
 
-    print("Enter second string")
+    println("Enter second string")
     //Read second string from the user and remove the space from the string
     val secondStr = readln().replace(" ","")
 
@@ -13,12 +26,23 @@ fun main() {
     //convert string to array of char
     val secondStrChar = secondStr.toCharArray()
 
-    //check length of string is equal or not
-    if(firstStr.length == secondStr.length){
+    var count = 0
 
-        for (i in 0..<firstStrChar.size){
-            firstStr[i]
+    //check length of string is equal or not (listen, silent)
+    if(firstStr.length == secondStr.length){
+        firstStrChar.forEach {
+            if(secondStrChar.contains(it)){
+                count++
+            }
         }
+
+
+        if(count == firstStrChar.size){
+            println("Given strings are anagram.")
+        }else {
+            println("Given strings are not anagram!")
+        }
+
     }else{
         println("Not an anagrams try others string...")
     }
