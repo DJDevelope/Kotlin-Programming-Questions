05ab7ec801c1e63a4a34edfca653de8e1dd36895
dhananjay <dhananjaysingh271@gmail.com>
2025-04-27 01:15:36 +0530

    Program to check missing numbers

--- app/src/main/java/com/example/programs/coding/find_missing_number.kt
+++ app/src/main/java/com/example/programs/coding/find_missing_number.kt
@@ -1,30 +1,30 @@
 fun main() {
-    println("Enter number followed by space to find missing number") // Print message to enter numbers
-    val numberStr = readln() // read input numbers from user
-    var numbers = numberStr.split(" ") // split the input string with space
-        .map { it.toInt() } // convert string to int
-        .toIntArray() // convert list to int array
+    println("Enter number followed by space to find missing number")
+    val numberStr = readln()
+    var numbers = numberStr.split(" ")
+        .map { it.toInt() }
+        .toIntArray()
 
-    var result = 0 // initialize result variable
-    var tempResult = 0 // initialize temp result
-    var missingValue = 0 // variable to store missing value
+    var result = 0
+    var tempResult = 0
+    var missingValue = 0
 
 
 
-    numbers.forEachIndexed { index, value -> // iterate through the array with index
+    numbers.forEachIndexed { index, value ->
 
-        if (index + 1 < numbers.size) { // check if index+1 is in bound
-            result = numbers[index + 1] - value // difference between next and current value
+        if (index + 1 < numbers.size) {
+            result = numbers[index + 1] - value
         }
 
-        if(tempResult != result && missingValue ==0){ // check if tempresult is not equals result
-            if(tempResult == 0 && tempResult != result){ // when temp result is 0 and result is not equal to 0
-                tempResult = result // initialize temp result with result
-            }else { // if tempresult is not 0
-                if(tempResult != 0 && tempResult < result){ // check if tempresult is less than result
-                    missingValue = value + (result - tempResult) // calculate the missing number
-                }else { // otherwise calculate the missing number
-                    missingValue = value + (tempResult - result) // calculate missing number
+        if(tempResult != result && missingValue ==0){
+            if(tempResult == 0){
+                tempResult = result
+            }else {
+                if(tempResult < result){
+                    missingValue = value + (result - tempResult)
+                }else {
+                    missingValue = value + (tempResult - result)
                 }
             }
         }
@@ -32,6 +32,6 @@ fun main() {
     }
 
 
-    println("Missing value is: ${missingValue}") // print the missing value
+    println("Missing value is: ${missingValue}")
 
 }
