8a9a348d7a87bc69b8b2074f79f45e4c0cb3ec54
dhananjay <dhananjaysingh271@gmail.com>
2025-04-26 22:45:12 +0530

    Program to check anagram of two strings using for loop.

--- app/src/main/java/com/example/programs/coding/check_anagram_using_loop.kt
+++ app/src/main/java/com/example/programs/coding/check_anagram_using_loop.kt
@@ -1,5 +1,32 @@
+//Check if two strings are Anagrams using for loop
+
 fun main() {
 
     println("Enter first string")
-    val firstStr = readLine()
+    val firstStr = readLine().toString().replace(" ", "")
+    println("Enter second string")
+    val secondStr = readLine().toString().replace(" ", "")
+
+    val firstStrToChar = firstStr.toCharArray()
+    val secondStrToChar = secondStr.toCharArray()
+
+    var count = 0
+
+    if(firstStrToChar.size == secondStrToChar.size){
+        for(i in 0..<firstStrToChar.size){
+            if(secondStrToChar.contains(firstStrToChar[i])){
+                count++
+            }
+        }
+
+        if(count == firstStrToChar.size){
+            println("String is anagram string")
+        }else{
+            println("String is not anagram string")
+        }
+    }else{
+        println("String is not anagram string")
+    }
+
+
 }
